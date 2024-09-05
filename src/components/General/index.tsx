b4cd058cb3adb5c3b0f5c38c13cc6e1ff25ba030
hugo emannuel <hugoemannuel53@gmail.com>
2024-09-05 14:11:30 -0300

    fix: adjustment compiling

--- src/components/General/index.tsx
+++ src/components/General/index.tsx
@@ -1,6 +1,6 @@
 import createIconsComponent from "../../helpers/createIconsComponent";
 import IconesGeneral from './json/Icons_General.json'
-import font from './fonts/Icons_general.ttf'
+import font from '../../fonts/Icons_general.ttf'
 
 const General = createIconsComponent("General", IconesGeneral, font)
 
